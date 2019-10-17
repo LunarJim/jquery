1df5e7a6d15af56640d42cf9ba0ddcc8f3c32018
Jérémie DEGHAYE <jeremiedeghaye@iMac-de-Jeremie-2.local>
2019-10-17 17:06:19 +0200

    test fonction search by 1st letter

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>jQuery</title>
 </head>
 <body>
@@ -19,7 +20,7 @@
                 <p class="content" id="content1">
                     the content pour une fois que je le tape
                 </p>
-                <button class="button" id="button1">the button</button>
+                <button class="button-delete" id="button1">the button</button>
             </div>
             <div class="article" id="art3">
                 <h2 class="title" id="title3">the title ouane</h2>
@@ -27,35 +28,35 @@
                 <p class="content" id="content3">
                     the content pour une fois que je le tape
                 </p>
-                <button class="button" id="button3">the button</button>
+                <button class="button-delete" id="button3">the button</button>
             </div><div class="article" id="art4">
                 <h2 class="title" id="title4">the title ouane</h2>
                 <span class="author" id="author4">the author</span>
                 <p class="content" id="content4">
                     the content pour une fois que je le tape
                 </p>
-                <button class="button" id="button4">the button</button>
+                <button class="button-delete" id="button4">the button</button>
             </div><div class="article" id="art5">
                 <h2 class="title" id="title5">the title ouane</h2>
                 <span class="author" id="author5">the author</span>
                 <p class="content" id="content5">
                     the content pour une fois que je le tape
                 </p>
-                <button class="button" id="button5">the button</button>
+                <button class="button-delete" id="button5">the button</button>
             </div><div class="article" id="art6">
                 <h2 class="title" id="title6">the title ouane</h2>
                 <span class="author" id="author6">the author</span>
                 <p class="content" id="content6">
                     the content pour une fois que je le tape
                 </p>
-                <button class="button" id="button6">the button</button>
+                <button class="button-delete" id="button6">the button</button>
             </div><div class="article" id="art7">
                 <h2 class="title" id="title7">the title ouane</h2>
                 <span class="author" id="author7">the author</span>
                 <p class="content" id="content7">
                     the content pour une fois que je le tape
                 </p>
-                <button class="button" id="button7">the button</button>
+                <button class="button-delete" id="button7">the button</button>
             </div>
             </div>
             <div class="cocktails">
@@ -69,10 +70,25 @@
                     <ul></ul>
                 </div>
             </div>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <div class="dropdown-menu alphabeticalSearch" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item alphabeticalItem" href="a">A</a>
+                    <a class="dropdown-item alphabeticalItem" href="b">B</a>
+                    <a class="dropdown-item alphabeticalItem" href="c">C</a>
+                </div>
+            </div>
         </div>
     </main>
+
     <footer>
     </footer>
+    
     <script src="js/main_script.js"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
--- js/main_script.js
+++ js/main_script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     console.log('init_OK');
 
     
-    $('.button').click(function(e) {
+    $('.button-delete').click(function(e) {
 
          var article = $(this.closest(".article"));
 
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
 
         $.ajax({
-            url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita',
+            url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a',
             type: 'GET',
             dataType: 'JSON',
             success: function(data) {
@@ -45,10 +45,47 @@ $(document).ready(function() {
 
         });
 
-        
-
-        
     });
+
+    $('.alphabeticalItem').click(function(e) {
+
+        e.preventDefault();
+
+        var selectedLetter = $(this).attr('href');
+
+        console.log(selectedLetter);
+
+        $.ajax({
+            url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + selectedLetter,
+            type: 'GET',
+            dataType: 'JSON',
+            success: function(data) {
+                
+                console.log('good');
+
+                $list = $('.list ul');
+
+                for (var index in data.drinks) {
+
+                var output = '';
+                output += '<li> ' + data.drinks[index].strDrink + data.drinks[index].strInstructions + '</li>';
+
+                $list.append(output);
+
+                };
+            
+                // var cocktailName = data.drinks[1].strDrink;
+
+                // $('.cocktails-name').after('<p> '+ cocktailName + ' </p>');
+            
+            }
+
+        });
+
+
+    })
+
+    
 
 
 });
